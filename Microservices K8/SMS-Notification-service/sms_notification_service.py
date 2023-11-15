156c6f4e3c83c3d221a6161371a0d4c2b72074df
Akeel Quamina <aquamina14@gmail.com>
2023-11-15 07:17:33 -0500

    UPDATED PY

--- Microservices K8/SMS-Notification-service/sms_notification_service.py
+++ Microservices K8/SMS-Notification-service/sms_notification_service.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 # Dummy database for storing notification preferences
 notification_preferences = {'user1': 'SMS', 'user2': 'Email', 'user3': 'SMS'}
 
-
 @app.route('/send-notification', methods=['POST'])
 def send_notification():
     data = request.get_json()
-
     user_id = data.get('user_id')
 
     if not user_id:
@@ -19,9 +17,7 @@ def send_notification():
         return jsonify({'error': 'User not found'}), 404
 
     notification_preference = notification_preferences[user_id]
-
     return jsonify({'message': f'Notification sent via {notification_preference} to user {user_id} successfully'})
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- Microservices K8/call-routing-service/call_routing_service.py
+++ Microservices K8/call-routing-service/call_routing_service.py
@@ -2,26 +2,22 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Dummy database for storing routing information
-routing_info = {'user1': 'route1', 'user2': 'route2', 'user3': 'route1'}
+# Dummy database for storing call routing preferences
+call_routing_preferences = {'user1': 'Route A', 'user2': 'Route B', 'user3': 'Route C'}
 
-
-@app.route('/call-routing', methods=['POST'])
+@app.route('/route-call', methods=['POST'])
 def route_call():
     data = request.get_json()
-
     user_id = data.get('user_id')
 
     if not user_id:
         return jsonify({'error': 'Invalid request'}), 400
 
-    if user_id not in routing_info:
+    if user_id not in call_routing_preferences:
         return jsonify({'error': 'User not found'}), 404
 
-    route = routing_info[user_id]
-
-    return jsonify({'message': f'Call routed through {route} for user {user_id} successfully'})
-
+    routing_preference = call_routing_preferences[user_id]
+    return jsonify({'message': f'Call routed via {routing_preference} for user {user_id}'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5003)
