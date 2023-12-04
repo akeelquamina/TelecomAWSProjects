d37b9b855c244ce636eaecef3245638e366b607b
Akeel Quamina <aquamina14@gmail.com>
2023-12-04 06:07:51 -0500

    updated staging information

--- Microservices_K8/k8s-deployments/Jenkinsfile.groovy
+++ Microservices_K8/k8s-deployments/Jenkinsfile.groovy
@@ -17,6 +17,33 @@ pipeline {
             }
         }
 
+        stage('Pull and Build Docker Images') {
+            steps {
+                script {
+                    withCredentials([usernamePassword(credentialsId: 'DockerHub_Connection', usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD'),
+                                     string(credentialsId: 'AWS_Access_Key_ID', variable: 'AWS_ACCESS_KEY_ID'),
+                                     string(credentialsId: 'AWS_Secret_Access_Key', variable: 'AWS_SECRET_ACCESS_KEY')]) {
+
+                        def services = ['billing-service', 'call-routing-service', 'sms-notification-service']
+
+                        // Iterate over services
+                        services.each { service ->
+                            def imageTag = "v1"
+                            def fullImageName = "akeelquamina/${service}:${imageTag}"
+
+                            // Pull Docker image or ignore failure
+                            sh "docker pull ${fullImageName} || true"
+
+                            // Build and push Docker image
+                            sh "docker buildx build -t ${fullImageName} ./Microservices_K8/${service}"
+                            sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
+                            sh "docker push ${fullImageName}"
+                        }
+                    }
+                }
+            }
+        }
+
         stage('Create EKS Cluster') {
             steps {
                 script {
