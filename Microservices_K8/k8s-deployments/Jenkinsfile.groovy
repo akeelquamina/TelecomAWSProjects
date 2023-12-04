77ca92e26991cb93a01162bf87347eb05f08024d
Akeel Quamina <aquamina14@gmail.com>
2023-12-04 05:45:09 -0500

    update

--- Microservices_K8/k8s-deployments/Jenkinsfile.groovy
+++ Microservices_K8/k8s-deployments/Jenkinsfile.groovy
@@ -16,41 +16,15 @@ pipeline {
             }
         }
 
-        stage('Pull and Build Docker Images') {
+        stage('Create EKS Cluster and Update Security Group') {
             steps {
                 script {
-                    withCredentials([usernamePassword(credentialsId: 'DockerHub_Connection', usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD'),
-                                     string(credentialsId: 'AWS_Access_Key_ID', variable: 'AWS_ACCESS_KEY_ID'),
-                                     string(credentialsId: 'AWS_Secret_Access_Key', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-
-                        def services = ['billing-service', 'call-routing-service', 'sms-notification-service']
-
-                        // Iterate over services
-                        services.each { service ->
-                            def imageTag = "v1"
-                            def fullImageName = "akeelquamina/${service}:${imageTag}"
-
-                            // Pull Docker image or ignore failure
-                            sh "docker pull ${fullImageName} || true"
-
-                            // Build and push Docker image
-                            sh "docker buildx build -t ${fullImageName} ./Microservices_K8/${service}"
-                            sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
-                            sh "docker push ${fullImageName}"
-                        }
-                    }
-                }
-            }
-        }
-
-        stage('Create EKS Cluster and Deploy to EKS') {
-            steps {
-                script {
+                   
                     // Read YAML file
                     def eksConfig = readYaml file: "Microservices_K8/k8s-deployments/eks-cluster.yml"
 
-                    // Create EKS Cluster
-                    sh "eksctl create cluster -f Microservices_K8/k8s-deployments/eks-cluster.yml"
+                    // Create EKS Cluster with the pre script
+                    sh "eksctl create cluster -f Microservices_K8/k8s-deployments/eks-cluster.yml --pre eksctl-pre-script.sh"
 
                     // Wait for the cluster to become ready
                     sh "eksctl utils wait --region=${AWS_DEFAULT_REGION} --for=cluster.active=${EKS_CLUSTER_NAME}"
